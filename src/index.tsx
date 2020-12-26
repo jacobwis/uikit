7d53b230289f0e3fc8adc2b080f546c3cc5e7e44
Jacob Wisniewski <wisniewskij26@gmail.com>
2020-12-26 14:37:22 -0600

    setup

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>Hello World</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, unde
+      quisquam praesentium sint nam repellat voluptatem veniam, harum adipisci
+      quidem iure numquam necessitatibus voluptatibus laudantium non accusantium
+      sunt illo nesciunt.
+    </p>
+  </div>
+);
+
+export default App;
