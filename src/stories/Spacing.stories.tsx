6fee61245cd78649ee5921be7935d31161ee5822
Jacob Wisniewski <wisniewskij26@gmail.com>
2020-12-27 12:28:24 -0600

    Update spacing

--- src/stories/Spacing.stories.tsx
+++ src/stories/Spacing.stories.tsx
@@ -19,174 +19,52 @@ export default {
 } as Meta;
 
 const Template: Story = () => {
-  const sizeKeys = Object.keys(spacing) as Array<keyof DefaultTheme['spacing']>;
   return (
     <div>
       <h1>Spacing</h1>
       <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="0" />
-        <p>0 - 0px</p>
+        <SpaceBox size="none" />
+        <p>none - 0rem - 0px</p>
       </div>
       <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
         <SpaceBox size="px" />
-        <p>px - 1px</p>
+        <p>px - 0.0625rem - 1px</p>
       </div>
       <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="0.5" />
-        <p>0.5 - 2px</p>
+        <SpaceBox size="xxxs" />
+        <p>xxxs - 0.125rem - 2px</p>
       </div>
       <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="1" />
-        <p>1 - 4px</p>
+        <SpaceBox size="xxs" />
+        <p>xxs - 0.25rem - 4px</p>
       </div>
       <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="1.5" />
-        <p>1.5 - 6px</p>
+        <SpaceBox size="xs" />
+        <p>xs - 0.5rem - 8px</p>
       </div>
       <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="2" />
-        <p>2 - 8px</p>
+        <SpaceBox size="sm" />
+        <p>sm - 0.75rem - 12px</p>
       </div>
       <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="2.5" />
-        <p>2.5 - 10px</p>
+        <SpaceBox size="md" />
+        <p>md - 1rem - 16px</p>
       </div>
       <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="3" />
-        <p>3 - 12px</p>
+        <SpaceBox size="lg" />
+        <p>lg - 1.5rem - 24px</p>
       </div>
       <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="3.5" />
-        <p>3.5 - 14px</p>
+        <SpaceBox size="xl" />
+        <p>xl - 2rem - 32px</p>
       </div>
       <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="4" />
-        <p>4 - 16px</p>
+        <SpaceBox size="xxl" />
+        <p>xxl - 2.5rem - 40px</p>
       </div>
-
       <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="5" />
-        <p>5 - 20px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="6" />
-        <p>6 - 24px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="7" />
-        <p>7 - 28px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="8" />
-        <p>8 - 32px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="9" />
-        <p>9 - 36px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="10" />
-        <p>10 - 40px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="11" />
-        <p>11 - 44px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="12" />
-        <p>12 - 48px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="14" />
-        <p>14 - 56px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="16" />
-        <p>16 - 64px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="20" />
-        <p>20 - 80px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="24" />
-        <p>24 - 96px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="28" />
-        <p>28 - 112px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="32" />
-        <p>32 - 128px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="36" />
-        <p>36 - 144px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="40" />
-        <p>40 - 160px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="44" />
-        <p>44 - 176px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="48" />
-        <p>48 - 192px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="52" />
-        <p>52 - 208px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="56" />
-        <p>56 - 224px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="60" />
-        <p>60 - 240px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="64" />
-        <p>64 - 256px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="72" />
-        <p>72 - 288px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="80" />
-        <p>80 - 320px</p>
-      </div>
-
-      <div style={{ display: 'flex', gap: '8px', marginBottom: '20px' }}>
-        <SpaceBox size="96" />
-        <p>96 - 384px</p>
+        <SpaceBox size="xxxl" />
+        <p>xxxl - 3rem - 48px</p>
       </div>
     </div>
   );
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,4 +1,3 @@
-// import original module declarations
 import 'styled-components';
 
 interface ColorMap {
@@ -14,7 +13,19 @@ interface ColorMap {
   900: string;
 }
 
-// and extend them!
+type SpacingUnit =
+  | 'none'
+  | 'px'
+  | 'xxxs'
+  | 'xxs'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | 'xxl'
+  | 'xxxl';
+
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
@@ -24,41 +35,7 @@ declare module 'styled-components' {
       green: ColorMap;
     };
     spacing: {
-      '0': string;
-      px: string;
-      '0.5': string;
-      '1': string;
-      '1.5': string;
-      '2': string;
-      '2.5': string;
-      '3': string;
-      '3.5': string;
-      '4': string;
-      '5': string;
-      '6': string;
-      '7': string;
-      '8': string;
-      '9': string;
-      '10': string;
-      '11': string;
-      '12': string;
-      '14': string;
-      '16': string;
-      '20': string;
-      '24': string;
-      '28': string;
-      '32': string;
-      '36': string;
-      '40': string;
-      '44': string;
-      '48': string;
-      '52': string;
-      '56': string;
-      '60': string;
-      '64': string;
-      '72': string;
-      '80': string;
-      '96': string;
+      [K in SpacingUnit]: string;
     };
   }
 }
